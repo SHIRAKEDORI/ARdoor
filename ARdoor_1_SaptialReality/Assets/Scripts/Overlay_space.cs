c36887c0461d843828ef85a0cec8bf1d5b745826
shirakedori <tksht261@gmail.com>
2021-02-04 02:49:53 +0900

    カメラ_というか画面座標系を得た　よし！

---
+++ ARdoor_1_SaptialReality/Assets/Scripts/Overlay_space.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Overlay_space : MonoBehaviour
+{
+    RectTransform rectTransform = null;
+    [SerializeField] Transform target = null;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        rectTransform.position = RectTransformUtility.WorldToScreenPoint(Camera.main, target.position);
+        Debug.Log(rectTransform.position);
+
+    }
+}
