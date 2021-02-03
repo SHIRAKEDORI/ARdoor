befdd1ef519df81be39e10048695925f19e642f7
shirakedori <tksht261@gmail.com>
2021-02-03 21:00:07 +0900

    カメラ座標系を得たと思ったらカメラの子の座標系だった　いみわからん

---
+++ ARdoor_1_SaptialReality/Assets/Scripts/Cameraspace.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class Cameraspace : MonoBehaviour
+{
+    RectTransform rectTransform = null;
+    [SerializeField] Transform target = null;
+
+    [SerializeField] Canvas canvas = null;
+
+    void Awake()
+    {
+
+        rectTransform = GetComponent<RectTransform>();
+        //canvas = GetComponent<Graphic>().canvas;
+        //canvas = GetComponent<Graphic>();
+    }
+
+    void Update()
+    {
+        var pos = Vector2.zero;
+        var uiCamera = Camera.main;
+        var worldCamera = Camera.main;
+        var canvasRect = canvas.GetComponent<RectTransform>();
+
+        var screenPos = RectTransformUtility.WorldToScreenPoint(worldCamera, target.position);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRect, screenPos, uiCamera, out pos);
+        rectTransform.localPosition = pos;
+        Debug.Log(pos);
+    }
+}
