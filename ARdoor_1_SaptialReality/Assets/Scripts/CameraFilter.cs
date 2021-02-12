6b7bbe452fa87636b9fc8a1ddac509d6d629665e
shirakedori <tksht261@gmail.com>
2021-02-13 02:17:48 +0900

    シェーダーかくらしい

---
+++ ARdoor_1_SaptialReality/Assets/Scripts/CameraFilter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class CameraFilter : MonoBehaviour
+{
+    [SerializeField] private Material filter;
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, filter);
+    }
+}
--- ARdoor_1_SaptialReality/Assets/Scripts/Overlay_space.cs
+++ ARdoor_1_SaptialReality/Assets/Scripts/Overlay_space.cs
@@ -5,17 +5,32 @@ using UnityEngine.UI;
 public class Overlay_space : MonoBehaviour
 {
     RectTransform rectTransform = null;
+    RectTransform rectTransform2 = null;
+    RectTransform rectTransform3 = null;
+    RectTransform rectTransform4 = null;
     [SerializeField] Transform target = null;
+    [SerializeField] Transform target2 = null;
+    [SerializeField] Transform target3 = null;
+    [SerializeField] Transform target4 = null;
 
     void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
+        rectTransform2 = GetComponent<RectTransform>();
+        rectTransform3 = GetComponent<RectTransform>();
+        rectTransform4 = GetComponent<RectTransform>();
     }
 
     void Update()
     {
         rectTransform.position = RectTransformUtility.WorldToScreenPoint(Camera.main, target.position);
         Debug.Log(rectTransform.position);
+        rectTransform2.position = RectTransformUtility.WorldToScreenPoint(Camera.main, target2.position);
+        Debug.Log(rectTransform2.position);
+        rectTransform3.position = RectTransformUtility.WorldToScreenPoint(Camera.main, target3.position);
+        Debug.Log(rectTransform3.position);
+        rectTransform4.position = RectTransformUtility.WorldToScreenPoint(Camera.main, target4.position);
+        Debug.Log(rectTransform4.position);
 
     }
 }
